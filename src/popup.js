8fb67a256959d9a2090d211e3b223499dc918688
Yuya Minami <yuya373@me.com>
2018-02-12 15:02:07 +0900

    rename to jsx

--- src/popup.js
+++ src/popup.js
@@ -1,5 +1,5 @@
 import React from "react";
 import { render } from "react-dom";
-import Popup from "./popup/index.js";
+import Popup from "./popup/index.js.jsx";
 
 render(<Popup />, document.getElementById("root"));
